c1bf7489b86251ea445193bc28b30e040dc5037d
Jorge Carlos <skay_1994@yahoo.com.br>
2024-04-19 16:40:34 -0300

    Feat: Create a class resolve to store instance and recovery

--- src/Container.php
+++ src/Container.php
@@ -2,9 +2,13 @@
 
 namespace Skay1994\MyFramework;
 
+use Skay1994\MyFramework\Container\Exceptions\ClassNotFound;
+
 class Container
 {
     public static ?Container $instance = null;
+
+    protected array $instances = [];
 
     public static function getInstance(): static
     {
@@ -13,5 +17,46 @@ class Container
         }
 
         return static::$instance;
+    }
+
+    public function flushAll(): void
+    {
+        $this->instances = [];
+    }
+
+    /**
+     * @throws ClassNotFound
+     */
+    public function resolve(string $class): mixed
+    {
+        $instance = static::getInstance();
+
+        if(!class_exists($class)) {
+            throw new ClassNotFound($class);
+        }
+
+        if($classInstance = self::get($class)) {
+            return $classInstance;
+        }
+
+        $newInstance = new $class;
+        $instance->instances[$class] = $newInstance;
+
+        return $newInstance;
+    }
+
+    /**
+     * @throws ClassNotFound
+     */
+    public static function make(string $class): mixed
+    {
+        return static::getInstance()->resolve($class);
+    }
+
+    public static function get(string $class)
+    {
+        $instance = static::getInstance();
+
+        return $instance->instances[$class] ?? null;
     }
 }
