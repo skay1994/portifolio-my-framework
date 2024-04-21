0fb4400b0a54a3d19cf7f298d2a8b0b91f63122a
Jorge Carlos <skay_1994@yahoo.com.br>
2024-04-21 14:41:06 -0300

    Feat: Add tests for Container class constructs parameters

--- tests/Container/ContainerTestsTest.php
+++ tests/Container/ContainerTestsTest.php
@@ -4,6 +4,47 @@ use Skay1994\MyFramework\Container;
 
 class DummyClass {}
 
+class DummyClassParameters {
+    public function __construct(
+        public DummyClass $dummyClass,
+        public ?string $name = null
+    )
+    {}
+}
+
+class DummyClassParameterWithRequired {
+    public function __construct(
+        public DummyClass $dummyClass,
+        public string $name,
+        public int $age,
+        public float $balance,
+        public bool $isActive,
+        public array $array,
+        public object $object
+    )
+    {}
+}
+
+class DummyClassParameterWithDefaultValue {
+    public function __construct(
+        public DummyClass $dummyClass,
+        public float $balance,
+        public bool $isActive = true,
+        public ?string $name = 'John Doe',
+        public ?int $age = 18,
+    )
+    {}
+}
+
+class DummyClassParametersExtra {
+    public function __construct(
+        public DummyClass $dummyClass,
+        public DummyClassParameters $dummyParameters,
+    )
+    {}
+}
+
+
 beforeEach(function () {
     Container::getInstance()->flushAll();
 });
@@ -45,3 +86,47 @@ it('Container throws exception if class not found', function () {
     Container\Exceptions\ClassNotFound::class,
     'Service Container: Class [\App\Class\NotFound] not found'
 );
+
+
+## Class constructor resolver
+
+it('Container resolve class constructors with parameters', function () {
+    $container = Container::make(DummyClassParameters::class);
+
+    expect($container)
+        ->toBeInstanceOf(DummyClassParameters::class)
+        ->and($container->dummyClass)->toBeInstanceOf(DummyClass::class)
+        ->and($container->name)->toBeNull();
+});
+
+it('Container resolve class constructors multiple with parameters', function () {
+    $container = Container::make(DummyClassParametersExtra::class);
+
+    expect($container)->toBeInstanceOf(DummyClassParametersExtra::class)
+        ->dummyParameters->dummyClass->toBeInstanceOf(DummyClass::class)
+        ->dummyClass->toBeInstanceOf(DummyClass::class)->toBe($container->dummyParameters->dummyClass);
+});
+
+it('Container resolve class constructor with non null parameters', function () {
+    $container = Container::make(DummyClassParameterWithRequired::class);
+
+    expect($container)
+        ->toBeInstanceOf(DummyClassParameterWithRequired::class)
+        ->dummyClass->toBeInstanceOf(DummyClass::class)
+        ->name->toBeString()->toBe('')
+        ->age->toBeInt()->toBe(0)
+        ->balance->toBeFloat()->toBe(0.0)
+        ->isActive->toBeBool()->toBeFalse()
+        ->array->toBeArray()->toHaveCount(0)
+        ->object->toBeObject()->toBeInstanceOf(stdClass::class);
+});
+
+it('Container resolve class constructor with default parameters value', function () {
+    $container = Container::make(DummyClassParameterWithDefaultValue::class);
+
+    expect($container)
+        ->toBeInstanceOf(DummyClassParameterWithDefaultValue::class)
+        ->name->toBe('John Doe')
+        ->age->toBe(18)
+        ->isActive->toBeTrue();
+});
