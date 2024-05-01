777ef8a6f4f14c79999f018e67d4061df8ee747c
Jorge Carlos <skay_1994@yahoo.com.br>
2024-05-01 01:37:54 -0300

    Chore: Create method put, to add routers on array

--- src/Router/RouteCollection.php
+++ src/Router/RouteCollection.php
@@ -5,4 +5,12 @@ namespace Skay1994\MyFramework\Router;
 class RouteCollection
 {
     protected static array $routes = [];
+
+    public function put(string $path, array $methods): void
+    {
+        self::$routes[] = [
+            'path' => $path,
+            'methods' => $methods
+        ];
+    }
 }
