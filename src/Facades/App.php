0e4262bc87872d44233ef841196036e9a61df8c3
Jorge Carlos <skay_1994@yahoo.com.br>
2024-05-01 01:57:47 -0300

    Chore: Add comment with new methods

--- src/Facades/App.php
+++ src/Facades/App.php
@@ -2,10 +2,17 @@
 
 namespace Skay1994\MyFramework\Facades;
 
+use Skay1994\MyFramework\Application;
 use Skay1994\MyFramework\Facade;
 
 /**
  * @method static run()
+ *
+ * @method static getRoutesPath()
+ * @method static basePath()
+ * @method static joinPaths()
+ *
+ * @see Application
  */
 class App extends Facade
 {
