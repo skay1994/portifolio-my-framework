9351a179419c9f03cdce984db6ac623388a4c161
Jorge Carlos <skay_1994@yahoo.com.br>
2024-05-01 01:39:08 -0300

    Chore: Create method parseURI to broken request URI in array

--- src/Router/RouteCollection.php
+++ src/Router/RouteCollection.php
@@ -13,4 +13,16 @@ class RouteCollection
             'methods' => $methods
         ];
     }
+
+    /**
+     * Parses the given URI and returns an array of its parts.
+     *
+     * @param string $uri The URI to be parsed.
+     * @return array The array of URI parts.
+     */
+    private function parserURI(string $uri): array
+    {
+        $uriParts = explode('/', $uri);
+        return array_filter($uriParts);
+    }
 }
