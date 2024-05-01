1b102059f8d701fc2e4bbfa7140a6d6b8b5f85f0
Jorge Carlos <skay_1994@yahoo.com.br>
2024-04-30 23:31:34 -0300

    Chore: Create helper to manipulate Filesystem Path

---
+++ src/Traits/FilesystemHelper.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Skay1994\MyFramework\Traits;
+
+trait FilesystemHelper
+{
+    /**
+     * Returns the base path of the application.
+     *
+     * @return string The base path of the application.
+     */
+    public function basePath(): string
+    {
+        return $this->app_path;
+    }
+
+    public function getRoutesPath(): string
+    {
+        return $this->joinPaths($this->app_path, 'src', 'http','controllers');
+    }
+
+    /**
+     * Joins multiple paths together to create a single path.
+     *
+     * @param string $basePath The base path to start with.
+     * @param string[] $paths The paths to append to the base path.
+     * @return string The resulting path after joining all the paths together.
+     *
+     * @link https://github.com/laravel/framework/blob/11.x/src/Illuminate/Filesystem/functions.php
+     */
+    public function joinPaths(string $basePath, ...$paths): string
+    {
+        foreach ($paths as $index => $path) {
+            if (empty($path)) {
+                unset($paths[$index]);
+            } else {
+                $paths[$index] = DIRECTORY_SEPARATOR.ltrim($path, DIRECTORY_SEPARATOR);
+            }
+        }
+
+        return $basePath.implode('', $paths);
+    }
+}
