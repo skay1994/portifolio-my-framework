162f0a4500bf554ca28e0a15f087deae6614c9ac
Jorge Carlos <skay_1994@yahoo.com.br>
2024-05-01 01:30:43 -0300

    Chore: Remove RouteFacade to create routers inside constructor

--- src/Attributes/Route.php
+++ src/Attributes/Route.php
@@ -8,8 +8,9 @@ use Skay1994\MyFramework\Facades\Route as RouteFacade;
 #[Attribute(Attribute::TARGET_CLASS|Attribute::TARGET_METHOD)]
 class Route
 {
-    public function __construct(string $path, array $methods = [])
-    {
-        RouteFacade::create($path, $methods);
-    }
+    public function __construct(
+        string $path,
+        array $methods = []
+    )
+    {}
 }
