32be67077068293d2cd133138a97d18bb6aaf263
Jorge Carlos <skay_1994@yahoo.com.br>
2024-05-02 17:45:09 -0300

    Chore: Create method to checks if the given namespace has any Route attributes.

--- src/Router/ClassHelper.php
+++ src/Router/ClassHelper.php
@@ -2,7 +2,37 @@
 
 namespace Skay1994\MyFramework\Router;
 
+use ReflectionClass;
+use ReflectionException;
+use Skay1994\MyFramework\Attributes\Route;
+
 trait ClassHelper
 {
+    /**
+     * Checks if the given namespace has any route attributes.
+     *
+     * @param string $namespace The namespace to check.
+     * @return bool True if the namespace has any route attributes, false otherwise.
+     */
+    public function hasAnyRouteAttribute(string $namespace): bool
+    {
+        try {
+            $reflection = new ReflectionClass($namespace);
+
+            $classAttribute = $reflection->getAttributes(Route::class);
+            $methodAttribute = false;
+
+            foreach ($reflection->getMethods() as $method) {
+                if ($method->getAttributes(Route::class)) {
+                    $methodAttribute = true;
+                    break;
+                }
+            }
+
+            return count($classAttribute) > 0 || $methodAttribute;
+        } catch (ReflectionException) {
+            return false;
+        }
+    }
 
 }
