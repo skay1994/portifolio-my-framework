62baa4bf94751320e86c6b80f6ebfddcb02b6802
Jorge Carlos <skay_1994@yahoo.com.br>
2024-05-04 13:09:32 -0300

    Chore: Add new methods on phpdoc

--- src/Facades/Container.php
+++ src/Facades/Container.php
@@ -5,12 +5,13 @@ namespace Skay1994\MyFramework\Facades;
 use Skay1994\MyFramework\Facade;
 
 /**
- * @method static bind(string $abstract, $concrete): void
  * @method static get(string $abstract): mixed
  * @method static make(string $abstract): mixed
  * @method static flushAll(): void
  * @method static resetDefault(): void
  * @method static setInstances(?array $instances = null): void
+ * @method static bind(string $abstract, $concrete): void
+ * @method static singleton(string $abstract, mixed $concrete): void
  *
  * @see \Skay1994\MyFramework\Container
  */
