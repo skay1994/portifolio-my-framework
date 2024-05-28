b2eedb1ef0b2dacefa69a21f9d648595a086a60b
Jorge Carlos <skay_1994@yahoo.com.br>
2024-05-28 14:10:48 -0300

    Chore: Added MultiReader to find env value in multiple readers

---
+++ src/Env/Readers/MultiReader.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Skay1994\MyFramework\Env\Readers;
+
+class MultiReader
+{
+    private array $readers = [
+        FileReader::class,
+        ServerConstReader::class,
+    ];
+
+    /**
+     * Retrieves the value associated with the given key from the readers.
+     *
+     * @param string $key The key to retrieve the value for.
+     * @return string|null The value associated with the key, or null if not found.
+     */
+    public function get(string $key): string|null
+    {
+        foreach ($this->readers as $reader) {
+            $reader = $reader::construct();
+
+            if ($reader->isSupported()) {
+                $value = $reader->get($key);
+
+                if ($value === false) {
+                    return 'false';
+                }
+
+                if ($value === true) {
+                    return 'true';
+                }
+
+                return (string) $value;
+            }
+        }
+
+        return null;
+    }
+}
