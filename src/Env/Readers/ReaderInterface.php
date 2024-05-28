48822ed060a06bc3040d7ebb5d3dd00d42fbac95
Jorge Carlos <skay_1994@yahoo.com.br>
2024-05-28 14:18:27 -0300

    Chore: Add construct and has methods on interface

--- src/Env/Readers/ReaderInterface.php
+++ src/Env/Readers/ReaderInterface.php
@@ -4,7 +4,11 @@ namespace Skay1994\MyFramework\Env\Readers;
 
 interface ReaderInterface
 {
+    public static function construct(): self;
+
     public function isSupported(): bool;
 
     public function get(string $key): mixed;
+
+    public function has(string $key): bool;
 }
