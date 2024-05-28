0defa581e4118d1ba55745842aacbe6faaa31a69
Jorge Carlos <skay_1994@yahoo.com.br>
2024-05-28 02:21:02 -0300

    Chore: Added __get method and phpdoc to access file details with class properties

--- src/Filesystem/File.php
+++ src/Filesystem/File.php
@@ -2,6 +2,15 @@
 
 namespace Skay1994\MyFramework\Filesystem;
 
+/**
+ * @property-read string $path
+ * @property-read string $name
+ * @property-read string $name_ext
+ * @property-read string $mimetype
+ * @property-read int $size
+ * @property-read mixed $content
+ * @property-read bool $exists
+ */
 readonly class File
 {
     public function __construct(
@@ -53,5 +62,24 @@ readonly class File
     public function read(): false|string
     {
         return file_get_contents($this->path);
+    }
+
+    public function exists(): bool
+    {
+        return file_exists($this->path);
+    }
+
+    public function __get(string $name)
+    {
+        return match ($name) {
+            'path' => $this->path,
+            'name' => $this->name(),
+            'name_ext' => $this->nameWithExtension(),
+            'mimetype' => $this->mimetype(),
+            'size' => $this->size(),
+            'content' => $this->read(),
+            'exists' => $this->exists(),
+            default => null
+        };
     }
 }
